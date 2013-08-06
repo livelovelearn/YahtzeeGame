400532692e5fcd35d2e2485c2ccd5e6599ea80e2
S <s>
2013-08-05 22:19:34 -0400

    {"type":"TI", "runs":"1"}

--- Yahtzee.java
+++ Yahtzee.java
@@ -29,9 +29,9 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 	}
 
 	private void playGame() {
-		int[] totalScore = new int[nPlayers+1];
+		int[] totalScore = new int[nPlayers + 1];
+		int[] upperScore = new int[nPlayers + 1];
 		for (int j = 0; j < N_SCORING_CATEGORIES; j++) {
-			
 			for (int i = 1; i <= nPlayers; i++) {
 				int player = i;
 				int[] dice = rollDice();
@@ -39,18 +39,44 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 				int category = display.waitForPlayerToSelectCategory();
 				boolean p = YahtzeeMagicStub.checkCategory(dice, category);
 				int score = getScore(p, category);
-				totalScore[i]+=score;
+				totalScore[i] += score;
 				display.updateScorecard(category, player, score);
-				display.updateScorecard(TOTAL, player, score);
+				display.updateScorecard(TOTAL, player, totalScore[i]);
+				
+				if (category<=SIXES){
+					upperScore[i] += score;
+					display.updateScorecard(UPPER_SCORE, player, upperScore[i]);
+				}
+				if (upperScore[i]>3){
+					display.updateScorecard(UPPER_BONUS, player, 35);
+					totalScore[i] += 35;
+					display.updateScorecard(TOTAL, player, totalScore[i]);
+				}
 			}
 		}
 	}
 
 	private int getScore(boolean match, int category) {
-		if (match) {
-			return 100;
+		if (!match) return 0;
+		switch(category){
+		ONES 
+		public static final int TWOS = 2;
+		public static final int THREES = 3;
+		public static final int FOURS = 4;
+		public static final int FIVES = 5;
+		public static final int SIXES = 6;
+		public static final int UPPER_SCORE = 7;
+		public static final int UPPER_BONUS = 8;
+		public static final int THREE_OF_A_KIND = 9;
+		public static final int FOUR_OF_A_KIND = 10;
+		public static final int FULL_HOUSE = 11;
+		public static final int SMALL_STRAIGHT = 12;
+		public static final int LARGE_STRAIGHT = 13;
+		public static final int YAHTZEE = 14;
+		public static final int CHANCE = 15;
+		public static final int LOWER_SCORE = 16;
+		public static final int TOTAL = 17;
 		}
-		return 0;
 	}
 
 	private int[] rollDice() {

bd2ea175786a3b9b6c96ce7066862f872e4b8f15
S <s>
2013-08-05 23:47:09 -0400

    {"type":"TI", "runs":"1"}

--- Yahtzee.java
+++ Yahtzee.java
@@ -38,16 +38,16 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 
 				int category = display.waitForPlayerToSelectCategory();
 				boolean p = YahtzeeMagicStub.checkCategory(dice, category);
-				int score = 100; //getScore(p, category);
+				int score = getScore(p, category, dice);
 				totalScore[i] += score;
 				display.updateScorecard(category, player, score);
 				display.updateScorecard(TOTAL, player, totalScore[i]);
-				
-				if (category<=SIXES){
+
+				if (category <= SIXES) {
 					upperScore[i] += score;
 					display.updateScorecard(UPPER_SCORE, player, upperScore[i]);
 				}
-				if (upperScore[i]>3){
+				if (upperScore[i] > 63) {
 					display.updateScorecard(UPPER_BONUS, player, 35);
 					totalScore[i] += 35;
 					display.updateScorecard(TOTAL, player, totalScore[i]);
@@ -56,28 +56,113 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 		}
 	}
 
-	/*private int getScore(boolean match, int category) {
-		if (!match) return 0;
-		switch(category){
-		/*ONES 
-		public static final int TWOS = 2;
-		public static final int THREES = 3;
-		public static final int FOURS = 4;
-		public static final int FIVES = 5;
-		public static final int SIXES = 6;
-		public static final int UPPER_SCORE = 7;
-		public static final int UPPER_BONUS = 8;
-		public static final int THREE_OF_A_KIND = 9;
-		public static final int FOUR_OF_A_KIND = 10;
-		public static final int FULL_HOUSE = 11;
-		public static final int SMALL_STRAIGHT = 12;
-		public static final int LARGE_STRAIGHT = 13;
-		public static final int YAHTZEE = 14;
-		public static final int CHANCE = 15;
-		public static final int LOWER_SCORE = 16;
-		public static final int TOTAL = 17;
-		}*/
+	private int getScore(boolean match, int category, int[] dice) {
+		if (!match)
+			return 0;
+		int score = 0;
+		int[] drawer = new int[6];
+		switch (category) {
+		case ONES:
+			for (int i = 0; i < N_DICE; i++)
+				if (dice[i] == 1)
+					score += 1;
+			return score;
+		case TWOS:
+			for (int i = 0; i < N_DICE; i++)
+				if (dice[i] == 2)
+					score += 2;
+			return score;
+		case THREES:
+			for (int i = 0; i < N_DICE; i++)
+				if (dice[i] == 3)
+					score += 3;
+			return score;
+		case FOURS:
+			for (int i = 0; i < N_DICE; i++)
+				if (dice[i] == 4)
+					score += 4;
+			return score;
+		case FIVES:
+			for (int i = 0; i < N_DICE; i++)
+				if (dice[i] == 5)
+					score += 5;
+			return score;
+		case SIXES:
+			for (int i = 0; i < N_DICE; i++)
+				if (dice[i] == 6)
+					score += 6;
+			return score;
+		case THREE_OF_A_KIND:
+			for (int i = 0; i < N_DICE; i++) {
+				drawer[dice[i]]++;
+				score += dice[i];
+			}
+			for (int i = 1; i <= 5; i++) {
+				if (drawer[i] >= 3)
+					return score;
+			}
+			return 0;
+		case FOUR_OF_A_KIND:
+			for (int i = 0; i < N_DICE; i++) {
+				drawer[dice[i]]++;
+				score += dice[i];
+			}
+			for (int i = 1; i <= 5; i++) {
+				if (drawer[i] >= 4)
+					return score;
+			}
+			return 0;
+		case FULL_HOUSE:
+			for (int i = 0; i < N_DICE; i++) {
+				drawer[dice[i]]++;
+				score += dice[i];
+			}
+			int emptyDrawer = 0;
+			for (int i = 1; i <= 5; i++) {
+				if (drawer[i] == 0)
+					emptyDrawer++;
+			}
+			for (int i = 1; i <= 5; i++) {
+				if (drawer[i] == 4)
+					emptyDrawer = -1;
+			}
+			if (emptyDrawer == 3)
+				return 25;
+			return 0;
+		case SMALL_STRAIGHT:
+			for (int i = 0; i < N_DICE; i++) {
+				drawer[dice[i]]++;
+			}
+			if( linked4(1, drawer)||linked4(2, drawer)||linked4(1, drawer))
+				return 30;
+			return 0;
+		case LARGE_STRAIGHT:
+			for (int i = 0; i < N_DICE; i++) {
+				drawer[dice[i]]++;
+			}
+			if( linked5(1, drawer)||linked5(2, drawer))
+				return 30;
+			return 0;
+		case YAHTZEE:
+			for (int i = 1; i < N_DICE; i++)
+				if (dice[0] != dice[i])
+					return 0;
+			return 50;
+
+		case CHANCE:
+			for (int i = 0; i < N_DICE; i++)
+				score += dice[i];
+			return score;
+
+		}
+	}
 	
+	private boolean linked4 (int start, int[] drawer){
+		return drawer[start++]==0||drawer[start++]==0||drawer[start++]==0||drawer[start++]==0;
+	}
+	private boolean linked5 (int start, int[] drawer){
+		return drawer[start++]==0||drawer[start++]==0||drawer[start++]==0||drawer[start++]==0||drawer[start++]==0;
+	}
 
 	private int[] rollDice() {
 		display.waitForPlayerToClickRoll(1);

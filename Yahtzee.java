afb8fd5e64ce55ae96caee7806f8627cb61d666d
Yancheng Liu <morningsherry@hotmail.com>
2013-10-27 19:39:53 -0400

    Update Yahtzee.java

--- Yahtzee.java
+++ Yahtzee.java
@@ -1,14 +1,10 @@
-/*
- * File: Yahtzee.java
- * ------------------
- * This program will eventually play the Yahtzee game.
- */
-
 import acm.io.*;
 import acm.program.*;
 import acm.util.*;
 
 public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
+
+	/*initiation according to the number of players*/
 
 	public static void main(String[] args) {
 		new Yahtzee().start(args);
@@ -16,20 +12,23 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 
 	public void run() {
 		IODialog dialog = getDialog();
-		nPlayers = dialog.readInt("Enter number of players");
+		nPlayers = dialog.readInt("Enter number of players"); // ask for the number of players
 		playerNames = new String[nPlayers];
 		for (int i = 1; i <= nPlayers; i++) {
 			playerNames[i - 1] = dialog.readLine("Enter name for player " + i);
+			// for each players, ask for their name
 		}
 		display = new YahtzeeDisplay(getGCanvas(), playerNames);
 		playGame();
 	}
 
 	private void playGame() {
+		// integer arrays to store scores of different categories 
 		int[] totalScore = new int[nPlayers + 1];
 		int[] upperScore = new int[nPlayers + 1];
+		// boolean arrays to store whether a category has been selected from each players
 		boolean[][] seletedCategory = new boolean[nPlayers + 1][N_CATEGORIES + 1];
-		// famehall[] fh = new famehall[10]; need to define outside this method
+		
 		int rank = 0;
 
 		for (int j = 0; j < N_SCORING_CATEGORIES; j++) {
@@ -38,23 +37,30 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 				int[] dice = rollDice(player);
 
 				int category = display.waitForPlayerToSelectCategory();
-				while (seletedCategory[i][category]) { // cannot select a
-														// selected category
+				while (seletedCategory[i][category]) { 
+					// cannot select a selected category
 					category = display.waitForPlayerToSelectCategory();
 				}
-				seletedCategory[i][category] = true;// set to selected
-				// boolean p = YahtzeeMagicStub.checkCategory(dice, category);
-				int score = getScore(category, dice);// calculate score of dice
-														// according to selected
-														// category
+				seletedCategory[i][category] = true;
+				// set to a previously unseleted category to selected
+				int score = getScore(category, dice);
+				/* calculate score of dice according scoring system of a selected category */
+				
 				totalScore[i] += score;
+				
+				// update total score after this round of seletion
+				
 				display.updateScorecard(category, player, score);
 				display.updateScorecard(TOTAL, player, totalScore[i]);
+				
+				// update upper score after this round of seletion if necessary
 
 				if (category <= SIXES) {
 					upperScore[i] += score;
 					display.updateScorecard(UPPER_SCORE, player, upperScore[i]);
 				}
+				
+				// checking conditions for upper bonus
 				if (upperScore[i] > 63) {
 					display.updateScorecard(UPPER_BONUS, player, 35);
 					totalScore[i] += 35;
@@ -62,19 +68,25 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 				}
 			}
 		}
-		/*
+		/* optional codes for fame hall 
+		
 		 * for (int i = 1; i <= nPlayers; i++) { for(int j=9;j>=0;j--){ if
 		 * (fh[j].score<totalScore[i]){ fh[j].score=totalScore[i]; rank=j+1;} }
 		 * display.printMessage("congraduations, "+" you are No."+ rank); }
 		 */
 	}
+	
+	/*
+	 * the method rollDice is responsible for generating 5 random number
+	 * between 1-6 each time the player role the dices
+	 */
 
 	private int[] rollDice(int player) {
 
-		display.waitForPlayerToClickRoll(player);
+		display.waitForPlayerToClickRoll(player); // wait for user response
 		int[] dice = new int[5];
 		for (int i = 0; i < 5; i++) {
-			dice[i] = rgen.nextInt(1, 6);
+			dice[i] = rgen.nextInt(1, 6); // generate random number mimicing dice rolling
 		}
 		display.displayDice(dice);
 		display.printMessage("1st time");
@@ -101,11 +113,15 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 		return dice;
 
 	}
+	/* the getScore() method translate the scoring system of each cagegory 
+	 * accofing to the game rule
+	 */
 
 	private int getScore(int category, int[] dice) {
 		int score = 0;
 		int[] drawer = new int[7];
 		switch (category) {
+			/* for ONE to SIXES, just add up the dices matching the category*/
 		case ONES:
 			for (int i = 0; i < N_DICE; i++)
 				if (dice[i] == 1)
@@ -136,6 +152,11 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 				if (dice[i] == 6)
 					score += 6;
 			return score;
+			/*
+			put all dices, based on their numbers, in the drawers labels 1-6 
+			if one of the draw has at least three dices, then add their number up
+			otherwise, player doesn't score in this category
+			*/
 		case THREE_OF_A_KIND:
 			for (int i = 0; i < N_DICE; i++) {
 				drawer[dice[i]]++;
@@ -146,6 +167,11 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 					return score;
 			}
 			return 0;
+			/*
+			put all dices, based on their numbers, in the drawers labels 1-6 
+			if one of the draw has at least four dices, then add their number up
+			otherwise, player doesn't score in this category
+			*/
 		case FOUR_OF_A_KIND:
 			for (int i = 0; i < N_DICE; i++) {
 				drawer[dice[i]]++;
@@ -156,6 +182,12 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 					return score;
 			}
 			return 0;
+			/*
+			put all dices, based on their numbers, in the drawers labels 1-6 
+			if more than four drawers are empty, the player score for fullhouse
+			except one condition need to be ruled out:
+			four dices of one kind and one dice of another
+			*/
 		case FULL_HOUSE:
 			for (int i = 0; i < N_DICE; i++) {
 				drawer[dice[i]]++;
@@ -173,6 +205,9 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 			if (emptyDrawer >= 4)
 				return 25;
 			return 0;
+			/*
+			test all the possibility for a linked 4 numbers from the dices
+			*/
 		case SMALL_STRAIGHT:
 			for (int i = 0; i < N_DICE; i++) {
 				drawer[dice[i]]++;
@@ -201,7 +236,9 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 			return 0;
 		}
 	}
-
+	/*
+	check whether 4 continuous drawers all have elements
+	*/
 	private boolean unlinked4(int start, int[] drawer) {
 		return drawer[start++] == 0 || drawer[start++] == 0
 				|| drawer[start++] == 0 || drawer[start++] == 0;
